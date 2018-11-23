83510e6c7b2da24204aed6131bd16d7fffbc99ff
fcocc77 <40272695+fcocc77@users.noreply.github.com>
2018-11-23 11:55:36 -0300

    Proxy Create

--- menu/menu_jc.py
+++ menu/menu_jc.py
@@ -2,12 +2,15 @@ import collect_files
 import card_to_track
 import reduce_project
 import edl_import
+import proxy_create
 
 menuBar = nuke.menu("Nuke")
 menuBar.addCommand('J_Script/CollectFiles...', 'collect_files.collect_files()', 'Ctrl+F')
 menuBar.addCommand('J_Script/Project Reduce.', 'reduce_project.reduceProject()', 'Ctrl+D')
 menuBar.addCommand('J_Script/CardtoTrack','card_to_track.corn3D()',icon="Camera.png")
 menuBar.addCommand('J_Script/Edl Import', 'edl_import.edlImport()')
+menuBar.addCommand('J_Script/Create Proxy', 'proxy_create.createProxy()')
+
 
 import autosave_backup
 autosave_backup.start_subprocess()
--- script/proxy_create.py
+++ script/proxy_create.py
@@ -1,5 +1,6 @@
 import os
 import re
+import nuke
 
 scale = 0.25
 
@@ -12,31 +13,32 @@ def postRender( _read, proxy ):
 	nuke.root()["proxy_type"].setValue(1)
 	nuke.root()["proxy_scale"].setValue(scale)
 
-for read in nuke.selectedNodes():
-	read["proxy"].setValue("")
+def createProxy():
+	for read in nuke.selectedNodes():
+		read["proxy"].setValue("")
 
-	# Nueva ruta creacion
-	file = read['file'].value()
-	basename = os.path.basename(file)
-	ext = basename.split(".")[-1]
-	padding = re.search('(#+)|(%\d\d?d)', file)
-	padding = padding.group(0) if padding else "" 
-	_basename = basename.split( padding )[0]
-	proxy_file = os.path.dirname(file) + "/" + _basename + "_proxy_" + padding + ".jpg"
-	#---------------------------------
+		# Nueva ruta creacion
+		file = read['file'].value()
+		basename = os.path.basename(file)
+		ext = basename.split(".")[-1]
+		padding = re.search('(#+)|(%\d\d?d)', file)
+		padding = padding.group(0) if padding else "" 
+		_basename = basename.split( padding )[0]
+		proxy_file = os.path.dirname(file) + "/" + _basename + "_proxy_" + padding + ".jpg"
+		#---------------------------------
 
-	reformat = nuke.createNode( "Reformat")
-	reformat["name"].setValue("reformat_proxy")
-	reformat["type"].setValue(2)
-	reformat["scale"].setValue(scale)
+		reformat = nuke.createNode( "Reformat")
+		reformat["name"].setValue("reformat_proxy")
+		reformat["type"].setValue(2)
+		reformat["scale"].setValue(scale)
 
-	write = nuke.createNode( "Write" )
-	write["name"].setValue("write_proxy")
-	write["file"].setValue(proxy_file)
-	write["_jpeg_quality"].setValue(1)
-	write["afterRender"].setValue("postRender(\"" + read["name"].value() + "\",\"" + proxy_file + "\")")
-	nuke.root().setProxy(False)
+		write = nuke.createNode( "Write" )
+		write["name"].setValue("write_proxy")
+		write["file"].setValue(proxy_file)
+		write["_jpeg_quality"].setValue(1)
+		write["afterRender"].setValue("postRender(\"" + read["name"].value() + "\",\"" + proxy_file + "\")")
+		nuke.root().setProxy(False)
 
-	nuke.execute(write,read.firstFrame(),read.lastFrame(),1)
+		nuke.execute(write,read.firstFrame(),read.lastFrame(),1)
 
-	
+		
